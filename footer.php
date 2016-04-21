b6484e8bb9991250cad3dacc606daa339611eac7
mekrh4n <318563@ogr.ktu.edu.tr>
2016-04-21 19:46:08 +0300

    anasayfa

---
+++ footer.php
@@ -0,0 +1,20 @@
+<div class="footer text-center">
+    <div class="bottom-menu">
+        <ul>
+            <li><a href="index.php">Haberler</a></li> |
+            <li><a href="sports.php">Spor</a></li> |
+            <li><a href="tech.php">Teknoloji</a></li> |
+            <li><a href="business.php">İş/Ekonomi</a></li> |
+            <li><a href="movies.php">Film</a></li> |
+            <li><a href="movies.php">Kültür</a></li> |
+            <li><a href="blog.php">Blog</a></li>
+        </ul>
+    </div>
+    <div class="copyright text-center">
+        <p>MEHMET ENES KÖRHAN</p>
+    </div>
+</div>
+</div>
+</div>
+</body>
+</html>
--- pages/anasayfa.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mekrh4n
- * Date: 4/20/16
- * Time: 6:44 PM
- */
